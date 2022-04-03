97c9d5efae8a8bc155ae9f165ef9b33bca04ffa5
Logan McAnsh <logan@mcan.sh>
2022-04-03 13:44:57 -0400

    chore: update turbo outputs
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- apps/remix-app/app/routes/index.tsx
+++ apps/remix-app/app/routes/index.tsx
@@ -2,8 +2,16 @@ import { Button } from "ui";
 
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix Turborepo Vercel example</h1>
+    <div
+      style={{
+        fontFamily: "system-ui, sans-serif",
+        lineHeight: "1.4",
+        padding: 20,
+      }}
+    >
+      <h1 style={{ fontWeight: "normal", margin: 0 }}>
+        Welcome to Remix + Turborepo + Vercel example
+      </h1>
       <Button />
     </div>
   );
